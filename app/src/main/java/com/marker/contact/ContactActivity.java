a6b5a5f1565f33b92675b415ed9d298a6f042e5d
FernandoVelcic <fvelcic@gmail.com>
2017-09-11 13:24:21 -0300

    Add back button in actionbar

--- app/src/main/java/com/marker/contact/ContactActivity.java
+++ app/src/main/java/com/marker/contact/ContactActivity.java
@@ -1,8 +1,10 @@
 package com.marker.contact;
 
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.marker.R;
 
@@ -11,6 +13,7 @@ public class ContactActivity extends AppCompatActivity implements ContactFragmen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupActionBar();
         setContentView(R.layout.activity_contact);
         getFragmentManager()
                 .beginTransaction()
@@ -21,4 +24,22 @@ public class ContactActivity extends AppCompatActivity implements ContactFragmen
     public void onListFragmentInteraction(Contact contact){
 
     };
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/marker/lugar/LugarActivity.java
+++ app/src/main/java/com/marker/lugar/LugarActivity.java
@@ -1,12 +1,16 @@
 package com.marker.lugar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 
 import com.marker.R;
+import com.marker.SettingsActivity;
 
 import java.util.Arrays;
 
@@ -23,6 +27,8 @@ public class LugarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupActionBar();
+
         setContentView(R.layout.activity_lugares);
         ButterKnife.bind(this);
         adapter = new LugaresRecyclerViewAdapter();
@@ -39,5 +45,23 @@ public class LugarActivity extends AppCompatActivity {
         };
 
         adapter.setItems(Arrays.asList(lugares));
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
