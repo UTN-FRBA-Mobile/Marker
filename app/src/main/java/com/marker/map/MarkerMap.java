fec82ccbebab9d4abb090439dcb942e81903f9ee
fran-bravo <fran_ase@hotmail.com>
2017-09-21 17:57:59 -0300

    Agregada actualizacion de posicion del fence visual

--- app/src/main/java/com/marker/map/MarkerMap.java
+++ app/src/main/java/com/marker/map/MarkerMap.java
@@ -73,6 +73,7 @@ public class MarkerMap {
     public void updateCamera(){
         map.moveCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 15.0f));
+        circle.setCenter(marker.getPosition());
     }
 
 
