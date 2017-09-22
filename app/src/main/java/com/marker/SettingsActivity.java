c5722b77ed1cb4aa522af7129fe1dc36370dc03e
FernandoVelcic <fernando_velcic_77@hotmail.com>
2017-09-21 23:48:17 -0300

    Fix button back in settings

--- app/src/main/java/com/marker/SettingsActivity.java
+++ app/src/main/java/com/marker/SettingsActivity.java
@@ -159,6 +159,16 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 || GeneralPreferenceFragment.class.getName().equals(fragmentName)
                 || NotificationPreferenceFragment.class.getName().equals(fragmentName);
     }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     /**
      * This fragment shows general preferences only. It is used when the
