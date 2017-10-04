630b2de9a4dce613ef879e33db6bebb2ed2790c4
Ezequiel Ayzenberg <ezequiel_ay@hotmail.com>
2017-10-04 16:50:19 -0300

    Prueba para obtener los amigos

--- app/src/main/java/com/marker/InicioFBActivity.java
+++ app/src/main/java/com/marker/InicioFBActivity.java
@@ -4,20 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenManager;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.gson.Gson;
+import com.marker.contact.FBUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class InicioFBActivity extends AppCompatActivity implements FacebookCallback<LoginResult> {
+public class InicioFBActivity extends AppCompatActivity implements FacebookCallback<LoginResult>, GraphRequest.Callback {
 
     @BindView(R.id.fb_login_button)
     protected LoginButton loginButton;
+    @BindView(R.id.lista_amigos)
+    protected TextView listaAmigos;
+
     private CallbackManager callbackManager;
 
     @Override
@@ -26,14 +41,24 @@ public class InicioFBActivity extends AppCompatActivity implements FacebookCallb
         setContentView(R.layout.activity_inicio_fb);
         ButterKnife.bind(this);
         loginButton.setReadPermissions("email");
+        loginButton.setReadPermissions("user_friends");
         callbackManager = CallbackManager.Factory.create();
         loginButton.registerCallback(callbackManager, this);
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        if (token != null) {
+            consultarAmigos(token);
+        }
     }
 
     @Override
     public void onSuccess(LoginResult loginResult) {
-        // App code
-        loginResult.hashCode();
+        AccessToken token = loginResult.getAccessToken();
+        consultarAmigos(token);
+    }
+
+    private void consultarAmigos(AccessToken token) {
+        new GraphRequest(token, "me/friends",
+                null, HttpMethod.GET, this).executeAsync();
     }
 
     @Override
@@ -55,5 +80,20 @@ public class InicioFBActivity extends AppCompatActivity implements FacebookCallb
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onCompleted(GraphResponse response) {
+        try {
+            JSONArray data = (JSONArray) response.getJSONObject().get("data");
+            FBUser[] amigos = new Gson().fromJson(data.toString(), FBUser[].class);
+            listaAmigos.setText("");
+            for (FBUser amigo : amigos) {
+                listaAmigos.append("\n"+amigo.getName());
+            }
+        } catch (JSONException e) {
+            //No tiene campo data
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/marker/contact/FBUser.java
@@ -0,0 +1,14 @@
+package com.marker.contact;
+
+public class FBUser {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
