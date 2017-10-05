c44e0fa87a7a2224262557798c981a1a626d2420
fran-bravo <fran_ase@hotmail.com>
2017-10-05 19:26:29 -0300

    Mejorado manejo de camara, extraida funcionalidad de location, armado menu de contactos trackeados y el resto no me acuerdo

--- app/src/main/java/com/marker/contact/Contact.java
+++ app/src/main/java/com/marker/contact/Contact.java
@@ -19,8 +19,8 @@ public class Contact implements Parcelable {
         this.checked = false;
     }
 
-    public static final List<Contact> initializeData(){
-        List<Contact> contacts = new ArrayList<>();
+    public static final ArrayList<Contact> initializeData(){
+        ArrayList<Contact> contacts = new ArrayList<>();
         contacts.add(new Contact("Eze", "1155555555", "eze@android.com"));
         contacts.add(new Contact("Fer", "1166666666", "fer@android.com"));
         contacts.add(new Contact("Fran", "1177777777", "fran@android.com"));
